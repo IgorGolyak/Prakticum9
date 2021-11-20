581617a8556806a93d7d1ec624b00893d5cef015
IgorGolyak <golyak.03@inbox.ru>
2021-11-20 11:06:58 +0700

    Prakticum9

---
+++ 18.11/18.11/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18._11
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.DialogResult == DialogResult.OK)
+            {
+                int k = 0;
+                try
+                {
+                    if (textBox2.Text == "")
+                    {
+                        textBox2.Focus();
+                        throw new Exception("Вы не ввели фио");
+                    }
+                    int r = Convert.ToInt32(textBox3.Text);
+                    k++;
+                    double v = Convert.ToDouble(textBox4.Text);
+
+                }
+                catch (FormatException)
+                {
+                    if (k == 0)
+                    {
+                        textBox3.Focus();
+                        MessageBox.Show("Рост число");
+                    }
+                    else if(k==1)
+                    {
+                        textBox4.Focus();
+                        MessageBox.Show("Вес число");
+                    }
+                    e.Cancel = true;
+                }
+                catch(Exception E)
+                {
+                    MessageBox.Show(E.Message);
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
---
+++ 18.11/18.11/People.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._11
+{
+    class People
+    {
+        public string FIO;
+        public double ves;
+        public int rost;
+
+        public override string ToString()
+        {
+            return FIO + "  рост=" + rost;
+        }
+    }
+}
